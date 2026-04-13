e72794c859d7e355cc7280e9c684cd6f1802c9b6
Rasmus Anthin <168433995+Captain-AdHoc@users.noreply.github.com>
2026-04-13 11:57:58 +0200

    Update key_counter_win.cpp

--- key_counter_win.cpp
+++ key_counter_win.cpp
@@ -114,7 +114,7 @@ namespace
     for (SHORT row = 3; row <= 10; ++row)
       write_text(console.handle, x, y + row, make_inner_line(L" ", inner_w), body_color);
 
-    write_text(console.handle, x, y + 10, L"└" + std::wstring(inner_w, L'─') + L"┘", border_color);
+    write_text(console.handle, x, y + 11, L"└" + std::wstring(inner_w, L'─') + L"┘", border_color);
   }
 
   double compute_keys_per_second(int moving_avg_window_ms)
